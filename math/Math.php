549ea93ba0100450720388b426dc21e031cad33b
navinweb <anatoliykirzo@gmail.com>
2018-08-14 22:55:54 +0300

    clearly static

--- math/Math.php
+++ math/Math.php
@@ -2,6 +2,9 @@
 
 class Math
 {
+
+  public static $sum = 0;
+
   public static function add() {
     return array_sum(func_get_args());
   }
@@ -9,9 +12,19 @@ class Math
   public function addAlternative(...$numbers) {
     return array_sum($numbers);
   }
+
+  public static function addSum() {
+    self::$sum += 1;
+  }
 }
 
 $math = new Math;
+// echo $math->addAlternative(1,2,2,3);
+// echo $math->add(1,2,2,3);
 
-echo Math::add(1,2,2,3);
-echo $math->addAlternative(1,2,2,3);
+// echo Math::add(1,2,2,3);
+// echo Math::addAlternative(1,2,2,3);
+
+$math::addSum();
+Math::addSum();
+echo Math::$sum;
